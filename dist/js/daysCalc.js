2cd66c8dfb3def71280dc07648a9839f8e46f45e
RandyRomero <a.ya.miheev@gmail.com>
2017-09-01 13:06:51 +0300

    set up Babel to transpile ES6 to ES5

---
+++ dist/js/daysCalc.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var tellMe = document.querySelector('main button');
+var submitButton = document.querySelector('#submit');
+var selectDay = document.querySelector('#day');
+var selectMonth = document.querySelector('#month');
+var selectYear = document.querySelector('#year');
+var form = document.querySelector('form');
+var output = document.querySelector('#output');
+var userDate = [null, null, null]; // [date, month, year]
+
+function evaluateDays() {
+    var now = new Date();
+    now.setFullYear(userDate[2], userDate[1], userDate[0]);
+    now.setHours(0, 0, 0, 0);
+    var birthday = new Date(now);
+    var days = Math.floor((new Date() - birthday) / (24 * 60 * 60 * 1000));
+    if (days < 0) {
+        output.textContent = 'You have not born yet.';
+    } else {
+        output.textContent = 'You were born ' + days + ' days ago.';
+    }
+}
+
+tellMe.addEventListener('click', function () {
+    // make visible form to choose birthday
+    tellMe.style.display = 'none';
+    form.style.display = 'block';
+    submitButton.style.display = 'block';
+});
+
+selectDay.addEventListener('change', function () {
+    // store day that user chose in from as soon as he chose it
+    userDate[0] = selectDay.value;
+});
+
+selectMonth.addEventListener('change', function () {
+    // ditto for month
+    userDate[1] = selectMonth.value - 1;
+});
+
+selectYear.addEventListener('change', function () {
+    // ditto for years
+    userDate[2] = selectYear.value;
+});
+
+submitButton.addEventListener('click', function () {
+    // check if parts of birthday are defined and starts calculator
+    var validDate = userDate.every(function (element) {
+        return !isNaN(parseFloat(element)) && isFinite(element);
+    });
+
+    if (!validDate) {
+        output.textContent = 'Choose day, month and year of your birthday first.';
+        return -1;
+    }
+    evaluateDays();
+});
+//# sourceMappingURL=daysCalc.js.map
